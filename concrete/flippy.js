6373805a79fe03b1dd652d71d4a910b078f4672a
Mike Frawley <frawl021@gmail.com>
2009-10-19 18:12:30 -0500

    add style and can flip cards up and down

--- concrete/flippy.js
+++ concrete/flippy.js
@@ -1,5 +1,17 @@
 $(document).ready(function ($) {
-  $('img').concrete({
+
+  $('.card').concrete({
+    onclick: function () {
+      this.flip();
+    },
+    flip: function () {
+      this.toggleClass('is-up');
+      console.log('flip! ' + this.name());
+    },
+    name: function () {
+      c = this.attr('class')
+      return this.attr('class').match(/\bcard-name-(.+?)\b/)[1];
+    },
     expand: function() {
       this.animate({height: 100}, {queue: false})
     },
