9997863683449fd9474110771127c1c3b3d1a37b
Mike Frawley <frawl021@gmail.com>
2009-10-20 00:12:25 -0500

    breaking up state in to different classes

--- concrete/flippy.js
+++ concrete/flippy.js
@@ -4,29 +4,41 @@ $(document).ready(function ($) {
 
 
 $('.card').concrete({
-  onclick: function () {
-    this.flip();
-  },
+  onclick: function () {  this.flip();  },
   flip: function () {
-    this.toggleClass('up').trigger('flip');
-  },
-  match: function () {
-    return this.map(this.name);
+    return this.toggleClass('up').toggleClass('active').trigger('flip');
   },
   name: function () {
     return this.attr('class').match(/\bcard-name-(.+?)\b/)[1];
-  }
+  },
+  expand: function() {
+    return this.animate({height: 100}, {queue: false})
+  },
+  collapse: function() {
+    return this.animate({height: 45}, {queue: false})
+  },
 });
 
+$('.card.up').concrete({
+  flip: function () {
+    this.trigger('_pointless_click');
+  },
+  mark_as_matched: function () {  return this.addClass('matched');  },
+  turn_down: function () {  return this.removeClass('up');  }
+});
+
+$('.card.up.active').concrete({
+  deactivate: function () {  return this.removeClass('active');  },
+  mark_as_matched: function () {  return this.deactivate()._super();  },
+  turn_down: function () {  return this.deactivate()._super();  }
+});
 
 $('.flipper').concrete({
   onflip: function () {
-    up = this.find('.card.up');
-    console.log(up.length);
-    
+    var up = this.find('.card.up.active');
     two_flipped_up()?  that_match()?  this.trigger('_match')
       :  this.trigger('_no_match')
-    :  0;
+    :  this.trigger('_first_flip');
     /////
     function two_flipped_up () { return up.length === 2; }
     function that_match () { return up.eq(0).name() === up.eq(1).name(); }
@@ -35,21 +47,37 @@ $('.flipper').concrete({
 
 
 $('.move').concrete({
-  on_match: function () {    
-    console.log(':)');
+  on_first_flip: function () { 
+    this.addClass('one-guess').find('.card:not(.up)').expand();
   },
-  on_no_match: function () {    
-    console.log(':(');    
-  },
-  
 });
 
 
-$('.game').concrete({
+$('.move.one-guess').concrete({
+  on_match: function () {  
+    console.log('match');
+    this.find('.card.up.active').matched();
+    this.start_state();
+  },
+  on_no_match: function () {  
+    console.log('no match');
+    this.find('.card.up.active').turn_down();
+    this.start_state();  
+  },
+  start_state: function () {  this.removeClass('one-guess');  },
+});
+
+
+
+$('*').concrete({
+  cards: function () {
+    this.find('.card');
+  }
 });
 
 $('.commentary').concrete({
   say: function (text) {  this.find('footer').text(text);  },
   on_match: function () {  this.say('nice');  },
   on_no_match: function () {  this.say('too bad');  },
+  on_pointless_click: function () {  this.say('Pick a face down card, bozo!'); }
 })
