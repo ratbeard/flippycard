a0cf0a34c5ed85f54d53e50c0871f6e093561b71
Mike Frawley <frawl021@gmail.com>
2009-10-20 10:58:38 -0500

    game working :)

--- concrete/flippy.js
+++ concrete/flippy.js
@@ -9,10 +9,11 @@ $('.card').concrete({
   animate_content: function (props, complete_fn) {
     var self = this; 
     return this.find('.content').
-      animate(props, {duration: 200, complete: function () {
-        console.log('done');
-        complete_fn.call(self);
-      }}).end();
+      animate(props, {
+        duration: 200, 
+        easing: 'swing',
+        complete: function () {  complete_fn.call(self);  }
+      }).end();
   },
   flip: function () {  
     return this.
@@ -29,50 +30,53 @@ $('.card').concrete({
 
 $('.game.on .card').concrete({
   onmatch: function () {  console.log('match');  },
-  flip: function () {  this.addClass('active')._super();  },
+  flip: function () {  this.toggleClass('guess')._super();  },
 });
 
 $('.game.on .card.up').concrete({
-  onmatch: function () {  console.log('match');  },
-  flip: function () {  this.trigger('_pointless_click');  },
-  mark_as_matched: function () {  return this.addClass('matched');  },
-  turn_down: function () {  return this.removeClass('up');  }
+  onmatch: function () {  console.log('a concrete match');  },
+  onclick: function () {  this.trigger('_pointless_click');  },
+  mark_as_matched: function () {  
+    return this.addClass('matched').removeClass('guess');  
+  },
 });
 
 $('.card.up.active').concrete({
-  deactivate: function () {  return this.removeClass('active');  },
   mark_as_matched: function () {  return this.deactivate()._super();  },
-  turn_down: function () {  return this.deactivate()._super();  }
 });
 
 $('.flipper').concrete({
   onflip: function () {
-    var active = this.find('.card.up.active');
-    two_flipped_up()?  that_match()?  this.trigger('_match')
-      :  this.trigger('_no_match')
-    :  this.trigger('_first_flip');
+    var guesses = this.find('.card.guess');
+    console.log('flipper: ',guesses.length);
+    
+    one_guess()?  this.trigger('_first_flip') 
+    : two_guesses()?  
+        that_match()?  this.trigger('_match')
+        : this.trigger('_no_match')
+    : 'flipped down' ;
     /////
-    function two_flipped_up () { return active.length === 2; }
-    function that_match () { return active.eq(0).name() === active.eq(1).name(); }
+    function one_guess () { return guesses.length === 1; }
+    function two_guesses () { return guesses.length === 2; }
+    function that_match () { return guesses.eq(0).name() === guesses.eq(1).name(); }
   }
 });
 
-
 $('.move').concrete({
-  on_first_flip: function () { 
+  on_first_flip: function () {
+    return this.addClass('one-guess');
   },
 });
-
 
 $('.move.one-guess').concrete({
   on_match: function () {  
     console.log('match');
-    this.find('.card.up.active').mark_as_matched();
+    this.find('.card.up.guess').mark_as_matched();
     this.start_state();
   },
   on_no_match: function () {  
     console.log('no match');
-    $('.card.up.active').flip();
+    $('.card.up.guess').flip();
     this.start_state();  
   },
   start_state: function () {  this.removeClass('one-guess');  },
@@ -90,5 +94,6 @@ $('.commentary').concrete({
   say: function (text) {  this.find('footer').text(text);  },
   on_match: function () {  this.say('nice');  },
   on_no_match: function () {  this.say('too bad');  },
+  on_first_flip: function () {  this.say('hmmm...');  },
   on_pointless_click: function () {  this.say('Pick a face down card, bozo!'); }
 })
